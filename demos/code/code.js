674cb557913f756e692f45cff6f99b60e2c02c6f
mirage335 <spamfreemirage335@gmail.com>
2018-04-28 05:31:23 -0400

    Change from SigBlockly.

--- demos/code/code.js
+++ demos/code/code.js
@@ -303,8 +303,8 @@ Code.renderContent = function() {
     var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
     xmlTextarea.value = xmlText;
     xmlTextarea.focus();
-  } else if (content.id == 'content_javascript') {
-    Code.attemptCodeGeneration(Blockly.JavaScript, 'js');
+  } else if (content.id == 'content_c') {
+    Code.attemptCodeGeneration(Blockly.c, 'c');
   } else if (content.id == 'content_python') {
     Code.attemptCodeGeneration(Blockly.Python, 'py');
   } else if (content.id == 'content_php') {
